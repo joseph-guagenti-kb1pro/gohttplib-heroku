688081d6b64074ed73ee84a18c7c200458a94ef1
Andrey Petrov <andrey.petrov@shazow.net>
2016-05-20 18:30:26 -0700

    Import

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from gohttp import route, run
+
+@route('/')
+def index(w, req):
+    w.write("%s %s %s\n" % (req.method, req.host, req.url))
+    w.write("Hello, world.\n")
+
+run(host='', port=os.environ.get('PORT', 5000))

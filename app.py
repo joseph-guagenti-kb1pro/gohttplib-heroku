60e3451e28cf4185e8b4501800ae94761656172a
Joseph Guagenti <joseph.guagenti@kb1pro.com>
2016-09-30 14:18:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from gohttp import route, run
 @route('/')
 def index(w, req):
     w.write("%s %s %s\n" % (req.method, req.host, req.url))
-    w.write("Hello, world (and Joe :).\n")
+    w.write("Hello, world (and Hans :).\n")
 
 run(host='', port=os.environ.get('PORT', 5000))
